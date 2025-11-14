741b9ea864bd96e1cd387bb1b4bc8fde3fe34149
Truong6886 <maiduytruong528@gmail.com>
2025-11-14 14:20:48 +0700

    update

--- src/components/QuanLyNhanVien.jsx
+++ src/components/QuanLyNhanVien.jsx
@@ -3,6 +3,7 @@ import Sidebar from "./Sidebar";
 import Header from "./Header";
 import EditProfileModal from "./EditProfileModal";
 import useDashboardData from "./CMSDashboard/hooks/useDashboardData";
+import translateService from "../utils/translateService";
 export default function QuanLyNhanVien() {
   const {showEditModal,setShowEditModal} = useDashboardData();
   const [currentUser, setCurrentUser] = useState(null);
@@ -50,21 +51,21 @@ export default function QuanLyNhanVien() {
     })();
   }, []);
 
-  const translateService = (serviceName) => {
-    const map = {
-      "인증 센터": "Chứng thực",
-      "결혼 이민": "Kết hôn",
-      "출생신고 대행": "Khai sinh, khai tử",
-      "출입국 행정 대행": "Xuất nhập cảnh",
-      "신분증명 서류 대행": "Giấy tờ tuỳ thân",
-      "입양 절차 대행": "Nhận nuôi",
-      "비자 대행": "Thị thực",
-      "법률 컨설팅": "Tư vấn pháp lý",
-      "B2B 서비스": "Dịch vụ B2B",
-      "기타": "Khác",
-    };
-    return map[serviceName] || serviceName;
-  };
+  // const translateService = (serviceName) => {
+  //   const map = {
+  //     "인증 센터": "Chứng thực",
+  //     "결혼 이민": "Kết hôn",
+  //     "출생신고 대행": "Khai sinh, khai tử",
+  //     "출입국 행정 대행": "Xuất nhập cảnh",
+  //     "신분증명 서류 대행": "Giấy tờ tuỳ thân",
+  //     "입양 절차 대행": "Nhận nuôi",
+  //     "비자 대행": "Thị thực",
+  //     "법률 컨설팅": "Tư vấn pháp lý",
+  //     "B2B 서비스": "Dịch vụ B2B",
+  //     "기타": "Khác",
+  //   };
+  //   return map[serviceName] || serviceName;
+  // };
 
   const statusOptions =
     currentLanguage === "vi"
